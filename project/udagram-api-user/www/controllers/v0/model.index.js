bde29764eca48e42f2d4beb1a92fd8c1e34ceeaa
kuchlong2 <kuchlong@gmail.com>
2021-10-24 01:26:27 -0500

    --

--- project/udagram-api-user/www/controllers/v0/model.index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.V0_USER_MODELS = void 0;
-const User_1 = require("./users/models/User");
-exports.V0_USER_MODELS = [User_1.User];
-//# sourceMappingURL=model.index.js.map
