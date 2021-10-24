2cacff7e16d3c2e713664a63f3a437aca181413c
kuchlong2 <kuchlong@gmail.com>
2021-10-24 01:37:53 -0500

    initial commit

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=da9
+export POSTGRES_PASSWORD=integra#90
+export POSTGRES_HOST=da6.cjkgekftdahs.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=da6-s3-dev
+export AWS_REGION=us-west-2
+export AWS_PROFILE=eksdude
+export JWT_SECRET=oafdjapjdapjgpj
+export URL=http://localhost:8100
+export PORT=8080
